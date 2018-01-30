5d8a69fb3f34c6e6157520119e718f14c58630ad
Dimitri Kontos <ckontos@rocketmail.com>
2018-01-30 03:04:26 +0000

    mobile responsive

--- public/js/initializations.js
+++ public/js/initializations.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.collapsible').collapsible();
+    $(".button-collapse").sideNav();
     $('.modal').modal();
+
 });
