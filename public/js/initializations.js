8a11a2bf47ac6f44a3d4a563d4191277dc3574c0
Dimitri Kontos <ckontos@rocketmail.com>
2018-03-14 00:22:17 +0000

    Fixed links and upgrades

--- public/js/initializations.js
+++ public/js/initializations.js
@@ -4,4 +4,24 @@ $(document).ready(function() {
     $(".button-collapse").sideNav();
     $('.modal').modal();
 
+    // start carrousel
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: false
+    });
+
+    // move next carousel
+    $('.moveNextCarousel').click(function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $('.carousel').carousel('next');
+    });
+
+    // move prev carousel
+    $('.movePrevCarousel').click(function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        $('.carousel').carousel('prev');
+    });
+
 });
