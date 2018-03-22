c45883401507f4a31af727bd674fd656349fd4ed
Dimitri Kontos <ckontos@rocketmail.com>
2018-03-22 01:59:44 +0000

    carosel looks better

--- public/js/initializations.js
+++ public/js/initializations.js
@@ -24,4 +24,19 @@ $(document).ready(function() {
         $('.carousel').carousel('prev');
     });
 
+    function setupEvents() {
+        if (typeof window.ontouchstart !== 'undefined') {
+            view.on('touchstart.carousel', tap);
+            view.on('touchmove.carousel', drag);
+            view.on('touchend.carousel', release);
+        }
+        view.on('mousedown.carousel', tap);
+        view.on('mousemove.carousel', drag);
+        view.on('mouseup.carousel', release);
+        view.on('mouseleave.carousel', release);
+        view.on('click.carousel', click);
+    }
+
+    setupEvents();
+
 });
